aa7ad5f48a5f10a0c24cdd8a1f972690a2069f14
julp <e2c2be7ed0f2f336@gmail.com>
2016-03-12 17:09:35 +0100

    check codepoint draft

--- lib/lexers/helpers.c
+++ lib/lexers/helpers.c
@@ -22,3 +22,43 @@ int named_elements_casecmp(const void *a, const void *b)
 
     return ascii_strcasecmp_l(na->name, na->name_len, nb->name, nb->name_len);
 }
+
+#define U_IS_SURROGATE(c) \
+    (0xD800 == ((c) & 0xFFFFF800))
+
+#define U_IS_SURROGATE_LEAD(c) \
+    (0 == ((c) & 0x400))
+
+#define U_IS_SURROGATE_TRAIL(c) \
+    (0 != ((c) & 0x400))
+
+/**
+ * Check if a code point is valid
+ *
+ * @param start
+ * @param end
+ * @param flags a bit mask of:
+ * - 16 to allow surrogates
+ * - 32 to forbid surrogates
+ *
+ * @return false on an invalid code point
+ */
+bool check_codepoint(const uint8_t *start, const uint8_t * const end, uint8_t flags)
+{
+    bool ok;
+    uint32_t cp;
+    const uint8_t *p;
+
+    cp = 0;
+    ret = true;
+    for (p = start; start < end; p++) {
+        cp = cp << 4 + ((*p >= '0' && *p <= '9') ? (*p - '0') : ((*p >= 'a' && *p <= 'f') ? (*p - 'a') : (*p - 'A'));
+    }
+    if (U_IS_SURROGATE(cp)) {
+        if (flags & 32) {
+            ret = false;
+        }
+    }
+
+    return ret;
+}
