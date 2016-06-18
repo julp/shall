310731c96e9672ea2ebc5b27cd0d7d6b73381c8c
julp <e2c2be7ed0f2f336@gmail.com>
2016-06-18 19:50:12 +0200

    uncomitted

--- shared/hashtable.h
+++ shared/hashtable.h
@@ -81,6 +81,9 @@ void *hashtable_first(HashTable *);
 void *hashtable_last(HashTable *);
 HashNode *hashtable_delete_node(HashTable *, HashNode *);
 
+HashTable *hashtable_copy(HashTable *, HashTable *, DupFunc, DupFunc);
+HashTable *hashtable_union(HashTable *, HashTable *, HashTable *, DupFunc, DupFunc);
+
 #define hashtable_hash(ht, k) \
     _hashtable_hash(ht, (ht_key_t) k)
 
